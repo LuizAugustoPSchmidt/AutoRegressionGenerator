e7a1504c2cd6a755dd03608fe217b47c170b723d
Luiz Schmidt <luizaugustops@gmail.com>
2023-11-02 20:10:55 -0300

    Implemented Linear Regression

--- Generator.c
+++ Generator.c
@@ -52,4 +52,40 @@ double getAvg(double* v, int size, int exponent){
     return (double)sumAll(v, size, exponent) / size;
 }
 
+double* linearRegression(double x[], double y[], int size){ //This could be fun to write in Haskell
+    const double avgX = getAvg(x, size, 1);
+    const double avgY = getAvg(y, size, 1);
 
+    double num = 0, den = 0;
+    /*regLinCoefs must be malloced in order to be able to return it, as you cannot return values allocated in the stack*/
+    double* regLinCoefs = (double*) malloc(2*sizeof(double)); //pos[0] is coef. "a" and pos[1] is coef. "b"
+
+    for(int i = 0; i < size; i++){
+        num += (x[i] - avgX)*(y[i] - avgY);
+        den += (x[i] - avgX)*(x[i] - avgX);
+    }
+
+    if(den){
+        regLinCoefs[0] = num / den;
+        regLinCoefs[1] = avgY - regLinCoefs[0] * avgX;
+        return regLinCoefs;
+    }else{
+        perror("Cannot divide by 0!");
+        free(regLinCoefs);
+        return NULL;
+    }
+}
+
+double* quadraticRegression(double x[], double y[], int size){}
+
+double* cubicRegression(double x[], double y[], int size){}
+
+double* exponentialRegression(double x[], double y[], int size){}
+
+double* logarithmicRegression(double x[], double y[], int size){}
+
+double* logisticalRegression(double x[], double y[], int size){}
+
+double* inverseRegression(double x[], double y[], int size){}
+
+double* inverseSquaredRegression(double x[], double y[], int size){}
